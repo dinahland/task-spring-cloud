a60f9cb16fd4cf56695bc7452300610984fe7a91
dinahland <hsunnies@gmail.com>
2025-07-31 14:58:35 +0900

    feat: Gateway에 CustomPreFilter, CustomPostFilter 추가

--- com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/controller/OrderController.java
+++ com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/controller/OrderController.java
@@ -1,21 +1,16 @@
 package com.sparta.msa_exam.order.controller;
 
-import com.sparta.msa_exam.order.service.OrderService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
+@RequestMapping("/order")
 public class OrderController {
 
-    private final OrderService orderService;
-
-    @GetMapping("/order/{orderId}")
-    public String getOrder(@PathVariable String orderId) {
-        return orderService.getOrder(orderId);
+    @GetMapping
+    public String getOrder() {
+        return "Order details";
     }
-
 }
 
--- com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/controller/ProductController.java
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/controller/ProductController.java
@@ -2,8 +2,8 @@ package com.sparta.msa_exam.product.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class ProductController {
@@ -11,10 +11,8 @@ public class ProductController {
     @Value("${server.port}") // 애플리케이션이 실행 중인 포트를 주입받습니다.
     private String serverPort;
 
-    @GetMapping("/product/{id}")
-    public String getProduct(@PathVariable String id) {
-        return "Product " + id + " info!!!!! From port : " + serverPort ;
+    @GetMapping("/product")
+    public String getProduct() {
+        return "Product info!!!!! From port : " + serverPort;
     }
-
-
 }
