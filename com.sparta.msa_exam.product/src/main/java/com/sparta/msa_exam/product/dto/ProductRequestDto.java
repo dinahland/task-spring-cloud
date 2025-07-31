694c5bc906632984fe7c8379c3042c83f9c48a30
dinahland <hsunnies@gmail.com>
2025-07-31 18:52:46 +0900

    feat: 상품 요청 Dto에 Validation 추가

--- com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/dto/ProductRequestDto.java
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/dto/ProductRequestDto.java
@@ -1,11 +1,14 @@
 package com.sparta.msa_exam.product.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class ProductRequestDto {
+    @NotBlank
     private String name;
+    @NotBlank
     private Integer supply_price;
 }
