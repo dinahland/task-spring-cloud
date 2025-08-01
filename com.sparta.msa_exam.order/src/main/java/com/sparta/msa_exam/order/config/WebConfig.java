481e88072174b1f21122cb154e3865844468bb7e
dinahland <hsunnies@gmail.com>
2025-08-01 21:53:45 +0900

    feat: 실행 서비스 포트 정보 응답 헤더로 반환 (인터셉터)

---
+++ com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.sparta.msa_exam.order.config;
+
+import com.sparta.msa_exam.order.handler.ServerPortInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final ServerPortInterceptor serverPortInterceptor;
+
+    public WebConfig(ServerPortInterceptor serverPortInterceptor) {
+        this.serverPortInterceptor = serverPortInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(serverPortInterceptor);
+    }
+}
+
---
+++ com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/handler/ServerPortInterceptor.java
@@ -0,0 +1,20 @@
+package com.sparta.msa_exam.order.handler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class ServerPortInterceptor implements HandlerInterceptor {
+
+    @Value("${server.port}")
+    private int serverPort;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        response.addHeader("Server-Port", String.valueOf(serverPort));
+        return true;
+    }
+}
