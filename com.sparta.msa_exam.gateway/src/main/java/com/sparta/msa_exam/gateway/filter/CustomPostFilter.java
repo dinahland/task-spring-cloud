9c9ff65d7bfb84ea12d67a522503db81c4abdc49
dinahland <hsunnies@gmail.com>
2025-08-01 19:30:27 +0900

    fix(gateway): 응답 헤더에 서버 포트 추가하는 로직 삭제 (인가 처리 로직과 충돌)

--- com.sparta.msa_exam.gateway/src/main/java/com/sparta/msa_exam/gateway/filter/CustomPostFilter.java
+++ com.sparta.msa_exam.gateway/src/main/java/com/sparta/msa_exam/gateway/filter/CustomPostFilter.java
@@ -2,14 +2,12 @@ package com.sparta.msa_exam.gateway.filter;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.net.URI;
 import java.util.logging.Logger;
 
 @Component
@@ -25,17 +23,6 @@ public class CustomPostFilter implements GlobalFilter, Ordered {
         return chain.filter(exchange).then(Mono.fromRunnable(() -> {
             ServerHttpResponse response = exchange.getResponse();
             logger.info("Post Filter: Response status code is " + response.getStatusCode());
-
-            // 응답 헤더에 실행 서비스 포트 추가
-            URI uri = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR);
-            if (uri != null && uri.getScheme().startsWith("http")) {
-                int port = uri.getPort();
-
-                // 포트 정보가 유효한 경우 헤더에 추가
-                if (port != -1) {
-                    response.getHeaders().add("Server-Port", String.valueOf(port));
-                }
-            }
         }));
     }
 
